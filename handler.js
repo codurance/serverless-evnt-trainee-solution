407f8d8dec82525cc840779350bc16f227a9cb46
Lau Llobet <lau.llobet@gmail.com>
2020-04-09 16:39:28 +0200

    # lambda does recive events

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.hello = async event => {
-  return {
+  console.log( JSON.stringify({
     statusCode: 200,
     body: JSON.stringify(
       {
@@ -11,7 +11,7 @@ module.exports.hello = async event => {
       null,
       2
     ),
-  };
+  } ));
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
