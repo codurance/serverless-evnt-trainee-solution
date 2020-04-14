ebacdcfa3b496742be9e6d0f0acedb609c40de12
Javier Chacana <javier.chacana@codurance.com>
2020-04-14 19:02:11 +0200

    Lambda persisting post into posts table

--- listening_lambda/handler.js
+++ listening_lambda/handler.js
@@ -1,20 +1,45 @@
 'use strict';
 
+const aws = require('aws-sdk')
+const parse = aws.DynamoDB.Converter.output;
+const ddb = new aws.DynamoDB({ apiVersion: '2012-08-10'})
+const docClient = new aws.DynamoDB.DocumentClient()
+
 module.exports.hello = async event => {
   console.log(event)
   console.log(event.detail)
-  console.log( JSON.stringify({
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  } ));
+  var body = event.detail
+  var params = {
+    TableName: 'posts',
+    Key: {
+      userId: body.userId
+    },
+    UpdateExpression: "SET #posts  = list_append(#posts, :post)",
+    ExpressionAttributeNames: { "#posts" : "posts"},
+    ExpressionAttributeValues: {":post": [event.detail]},
+    ReturnValues : "UPDATED_NEW"
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  }
+
+  return await new Promise((resolve, reject) => {
+    docClient.update(params, (err, data) => {
+      console.log("@ docClient update")
+      if (err) {
+        console.log("Error", err)
+        reject()
+      } else {
+        console.log("I got a response ")
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(
+            parse({ "M": data.Item}).posts,
+            null,
+            2
+          )
+        }
+        console.log(response);
+        return callback(null, response)
+      }
+    })
+  })
 };
