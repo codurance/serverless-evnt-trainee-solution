82ee29fc99792294ae18c10cbbe83e6af3c1e511
Javier Chacana <javier.chacana@codurance.com>
2020-04-20 15:25:16 +0200

    Added logic to get the list of followers the post's creator has

--- listening_lambda/handler.js
+++ listening_lambda/handler.js
@@ -28,4 +28,23 @@ module.exports.materialize = async event => {
   console.log("Materialize event", event);
   var post = event.detail
   console.log(post)
+  const followers = await getFollowers(post)
+  followers.forEach((follower, index) => {
+    console.log("Going to add post to " + followers + "'s timeline")
+  })
 };
+
+
+async function getFollowers(post) {
+  var params = {
+    TableName: 'followed',
+    Key: {
+      userId: post.userId
+    },
+    ProjectionExpression: 'followedBy'
+  }
+
+  const docClient = new aws.DynamoDB.DocumentClient()
+  const result = await docClient.get(params).promise()
+  return result.Item.followedBy
+}
