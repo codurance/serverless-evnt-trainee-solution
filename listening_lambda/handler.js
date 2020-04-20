ad8f4048aa4b53e4cb2c990960b049905acf332d
Javier Chacana <javier.chacana@codurance.com>
2020-04-20 15:56:25 +0200

    Added logic to add post to each follower's timeline

--- listening_lambda/handler.js
+++ listening_lambda/handler.js
@@ -29,9 +29,11 @@ module.exports.materialize = async event => {
   var post = event.detail
   console.log(post)
   const followers = await getFollowers(post)
-  followers.forEach((follower, index) => {
-    console.log("Going to add post to " + followers + "'s timeline")
+  var promises = []
+  followers.forEach((follower, index) => { 
+    promises.push(addToTimeline(followers, post))
   })
+  return Promise.all(promises)
 };
 
 
@@ -47,4 +49,28 @@ async function getFollowers(post) {
   const docClient = new aws.DynamoDB.DocumentClient()
   const result = await docClient.get(params).promise()
   return result.Item.followedBy
+}
+
+async function addToTimeline(follower, post) {
+  console.log("Adding post to " + follower + "'s timeline")
+  return addPost(post, follower, 'timeline')
+}
+
+
+async function addPost(post, user, table) {
+  console.log("Inside addPost for ", post, user, table)
+  var params = {
+    TableName: table,
+    Key: {
+      userId: user
+    },
+    UpdateExpression: "SET #posts  = list_append(#posts, :post)",
+    ExpressionAttributeNames: { "#posts" : "posts"},
+    ExpressionAttributeValues: {":post": [post]},
+    ReturnValues : "UPDATED_NEW"
+  }
+
+  console.log("addPost params", params)
+  return await docClient.update(params).promise();
+
 }
